48403ef47b0d96eb526b35a68dfd25babd0c7004
Felipe <felipe@visionar.io>
2024-12-02 21:08:37 -0300

    add product item component

---
+++ lib/componensts/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:ecomm/models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite),
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecomm/componensts/product_item.dart';
 import 'package:ecomm/models/product.dart';
 import 'package:flutter/material.dart';
 import '../data/dummy_data.dart';
@@ -13,18 +14,16 @@ class ProductsOverviewPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Store'),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (ctx, idx) => Text(loadedProducts[idx].title),
+        itemCount: loadedProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemBuilder: (ctx, idx) => ProductItem(product: loadedProducts[idx]),
       ),
     );
   }
