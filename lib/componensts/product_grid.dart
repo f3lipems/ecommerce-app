bea2ad508c445cdfa0fd30326f966b9b7d3638f9
Felipe <felipe@visionar.io>
2024-12-05 20:10:28 -0300

    refactor product grid

---
+++ lib/componensts/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:ecomm/componensts/product_item.dart';
+import 'package:ecomm/models/product.dart';
+import 'package:ecomm/models/product_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+    final List<Product> loadedProducts = provider.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: loadedProducts.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemBuilder: (ctx, idx) => ProductItem(product: loadedProducts[idx]),
+    );
+  }
+}
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,32 +1,18 @@
-import 'package:ecomm/componensts/product_item.dart';
-import 'package:ecomm/models/product.dart';
-import 'package:ecomm/models/product_list.dart';
+import 'package:ecomm/componensts/product_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Store'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, idx) => ProductItem(product: loadedProducts[idx]),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
+
+
