0caeab396b22b624e7b053fe3f05862f7dbf8e17
Felipe <felipe@visionar.io>
2024-12-09 18:01:46 -0300

    add filter of items

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,7 +5,24 @@ import 'package:flutter/material.dart';
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
 
-  List<Product> get items => [..._items];
+  List<Product> get items {
+    if (_showFavoriteOnly) {
+      return _items.where((product) => product.isFavorite).toList();
+    }
+    return [..._items];
+  }
+
+  bool _showFavoriteOnly = false;
+
+  void showFavoriteOnly() {
+    _showFavoriteOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavoriteOnly = false;
+    notifyListeners();
+  }
 
   void addProduct(Product product) {
     _items.add(product);
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,18 +1,46 @@
 import 'package:ecomm/componensts/product_grid.dart';
+import 'package:ecomm/models/product_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOptions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Store'),
+        actions: [
+          PopupMenuButton(
+              icon: const Icon(Icons.more_vert),
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.Favorite) {
+                  provider.showFavoriteOnly();
+                } else {
+                  provider.showAll();
+                }
+              },
+              itemBuilder: (_) => [
+                    const PopupMenuItem(
+                      value: FilterOptions.Favorite,
+                      child: Text('Favoritos'),
+                    ),
+                    const PopupMenuItem(
+                      value: FilterOptions.All,
+                      child: Text('Todos'),
+                    ),
+                  ])
+        ],
       ),
       body: const ProductGrid(),
     );
   }
 }
-
-
