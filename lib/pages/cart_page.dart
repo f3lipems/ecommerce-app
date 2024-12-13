30ea09e96954d45f0e69b0ac097b7f3078a9697b
Felipe <felipe@visionar.io>
2024-12-13 18:45:36 -0300

    add hidden current snackbar

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,7 +1,6 @@
 import 'package:ecomm/componensts/cart_item.dart';
 import 'package:ecomm/models/cart.dart';
 import 'package:ecomm/models/order_list.dart';
-import 'package:ecomm/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
