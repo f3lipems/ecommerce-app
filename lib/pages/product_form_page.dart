86a8b2c30a8a7357fb1d2b10b204bac7dafc97d3
Felipe <felipe@visionar.io>
2024-12-17 11:26:40 -0300

    add submit on product form page

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,3 +1,7 @@
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:ecomm/models/product.dart';
 import 'package:flutter/material.dart';
 
 class ProductFormPage extends StatefulWidget {
@@ -12,6 +16,8 @@ class _ProductFormPageState extends State<ProductFormPage> {
   final _descriptionFocus = FocusNode();
   final _imageUrlFocus = FocusNode();
   final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void initState() {
@@ -32,84 +38,107 @@ class _ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
+  void _submitForm() {
+    _formKey.currentState?.save();
+    final newProduct = Product (
+      id: Random().nextDouble().toString(),
+      name: _formData['name'] as String,
+      price: _formData['price'] as double,
+      description: _formData['description'] as String,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulário de Produto'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: _submitForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+            key: _formKey,
             child: ListView(
-          children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Nome'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_priceFocus);
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Preço'),
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              textInputAction: TextInputAction.next,
-              focusNode: _priceFocus,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_descriptionFocus);
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'Descrição'),
-              textInputAction: TextInputAction.next,
-              focusNode: _descriptionFocus,
-              keyboardType: TextInputType.multiline,
-              maxLines: 3,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(labelText: 'URL da Imagem'),
-                    textInputAction: TextInputAction.done,
-                    focusNode: _imageUrlFocus,
-                    keyboardType: TextInputType.url,
-                    controller: _imageUrlController,
-                  ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocus);
+                  },
+                  onSaved: (name) => _formData['name'] = name ?? '',
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 10),
-                  width: 100,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1,
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Preço'),
+                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _priceFocus,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_descriptionFocus);
+                  },
+                  onSaved: (price) => _formData['price'] = double.parse(price ?? '0'),
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Descrição'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _descriptionFocus,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  onSaved: (description) => _formData['description'] = description ?? '',
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'URL da Imagem'),
+                        textInputAction: TextInputAction.done,
+                        focusNode: _imageUrlFocus,
+                        keyboardType: TextInputType.url,
+                        controller: _imageUrlController,
+                        onFieldSubmitted: (_) => _submitForm(),
+                        onSaved: (imageUrl) => _formData['imageUrl'] = imageUrl ?? '',
+                      ),
                     ),
-                  ),
-                  child: _imageUrlController.text.isEmpty
-                      ? const Center(
-                          child: Icon(
-                            Icons.image,
-                            size: 50,
-                          ),
-                        )
-                      : FittedBox(
-                          child: Image.network(
-                            _imageUrlController.text,
-                            fit: BoxFit.cover,
-                          ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, left: 10),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1,
                         ),
+                      ),
+                      child: _imageUrlController.text.isEmpty
+                          ? const Center(
+                              child: Icon(
+                                Icons.image,
+                                size: 50,
+                              ),
+                            )
+                          : FittedBox(
+                              child: Image.network(
+                                _imageUrlController.text,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                    ),
+                  ],
                 ),
+                // ElevatedButton(
+                //   onPressed: () {},
+                //   child: const Text('Salvar'),
+                // ),
               ],
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Salvar'),
-            ),
-          ],
-        )),
+            )),
       ),
     );
   }
