ab371f3a9cc969a62ccad4fbb72fcabc8647c8a9
Felipe <felipe@visionar.io>
2024-12-23 15:12:57 -0300

    add orders on firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,10 +1,12 @@
-import 'dart:math';
-
+import 'dart:convert';
+import 'package:ecomm/utils/constants.dart';
+import 'package:http/http.dart' as http;
 import 'package:ecomm/models/cart.dart';
 import 'package:ecomm/models/order.dart';
 import 'package:flutter/material.dart';
 
-class OrderList with ChangeNotifier{
+class OrderList with ChangeNotifier {
+  final _baseUrl = 'https://ecomm-flutterlab-default-rtdb.firebaseio.com';
 
   List<Order> _orders = [];
 
@@ -12,16 +14,47 @@ class OrderList with ChangeNotifier{
 
   int get itemsCount => _orders.length;
 
-  void addOrder(Cart cart){
-    _orders.insert(0, 
-      Order(
-        id: Random().nextDouble().toString(),
-        amount: cart.totalAmount,
-        date: DateTime.now(),
-        products: cart.items.values.toList(),
-      )
+  Future<void> addOrder(Cart cart) async {
+    final url = '$_baseUrl/orders.json';
+    final date = DateTime.now();
+    final dateIso = DateTime.now().toIso8601String();
+    final productsMap = cart.items.values
+        .map((cartItem) => {
+              'id': cartItem.id,
+              'productId': cartItem.productId,
+              'name': cartItem.name,
+              'quantity': cartItem.quantity,
+              'price': cartItem.price,
+            })
+        .toList();
+
+    print(url);
+
+    final postResponse = await http.post(
+      Uri.parse(url),
+      body: json.encode(
+        {
+          'total': cart.totalAmount,
+          'date': dateIso,
+          'products': productsMap,
+        },
+      ),
     );
+
+    if (postResponse.statusCode != 200) {
+      throw Exception('Erro ao cadastrar pedido');
+    }
+
+    final id = json.decode(postResponse.body)['name'] as String;
+
+    _orders.insert(
+        0,
+        Order(
+          id: id,
+          amount: cart.totalAmount,
+          date: date,
+          products: cart.items.values.toList(),
+        ));
     notifyListeners();
   }
-  
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const productsBaseUrl = 'https://flutter-shop-7b3b4-default-rtdb.firebaseio.com/products';
+  static const ordersBaseUrl = 'https://flutter-shop-7b3b4-default-rtdb.firebaseio.com/orders';
+}
