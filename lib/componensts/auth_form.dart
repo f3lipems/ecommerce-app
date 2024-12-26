c4ef8c1ceff4da6d9febd4b6aff71607f0870a6e
Felipe <felipe@visionar.io>
2024-12-26 08:27:15 -0300

    add auth form fields

--- lib/componensts/auth_form.dart
+++ lib/componensts/auth_form.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
+enum AuthMode { Signup, Login }
+
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
 
   @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  AuthMode _authMode = AuthMode.Login;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Container(
+        height: 320,
+        width: deviceSize.width * 0.8,
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: Column(
+            children: [
+              const TextField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              const TextField(
+                decoration: InputDecoration(labelText: 'Senha'),
+                obscureText: true,
+              ),
+              if (_authMode == AuthMode.Signup)
+                const TextField(
+                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                  obscureText: true,
+                ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
