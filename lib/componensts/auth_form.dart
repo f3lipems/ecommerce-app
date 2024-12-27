d7ee8c043a1ac3b21fe19fdff0a64884a89ff5c3
Felipe <felipe@visionar.io>
2024-12-26 23:00:42 -0300

    add actions on form component

--- lib/componensts/auth_form.dart
+++ lib/componensts/auth_form.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 enum AuthMode { Signup, Login }
@@ -10,7 +12,14 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
-  AuthMode _authMode = AuthMode.Login;
+  final AuthMode _authMode = AuthMode.Login;
+  final _passwordController = TextEditingController();
+  final Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+
+  void _submit() {}
 
   @override
   Widget build(BuildContext context) {
@@ -21,29 +30,67 @@ class _AuthFormState extends State<AuthForm> {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Container(
-        height: 320,
         width: deviceSize.width * 0.8,
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 28),
         child: Form(
           child: Column(
             children: [
-              const TextField(
-                decoration: InputDecoration(labelText: 'E-mail'),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
                 keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (emailField) {
+                  if (emailField == null || emailField.isEmpty || !emailField.contains('@')) {
+                    return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
               ),
-              const TextField(
-                decoration: InputDecoration(labelText: 'Senha'),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Senha'),
                 obscureText: true,
+                controller: _passwordController,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: (passwordField) {
+                  if (passwordField == null || passwordField.isEmpty) {
+                    return 'Informe uma senha válida';
+                  } else if (passwordField.length <= 5) {
+                    return 'Senha precisa ter no mínimo 6 caracteres';
+                  }
+                  return null;
+                },
               ),
               if (_authMode == AuthMode.Signup)
-                const TextField(
-                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Confirmar Senha'),
                   obscureText: true,
+                  validator: _authMode == AuthMode.Login
+                      ? null
+                      : (confirmPasswordField) {
+                          final password = confirmPasswordField ?? '';
+                          if (password != _passwordController.text) {
+                            return 'Senhas não conferem';
+                          }
+                          return null;
+                        },
                 ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text('Entrar'),
+                onPressed: _submit,
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 8,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+                child: Text(
+                  _authMode == AuthMode.Login ? 'Entrar' : 'Registrar',
+                  style: const TextStyle(color: Colors.white),
+                ),
               ),
             ],
           ),
