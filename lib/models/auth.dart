7ffb0d98902d5283b754ef38e436886e43dc0d2f
Felipe <felipe@visionar.io>
2025-01-12 14:46:36 -0300

    add getters on auth model

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -4,8 +4,30 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-  static const _url ='https://identitytoolkit.googleapis.com/v1/accounts:';
+  static const _url = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   static const _keyUrlSegment = '?key=AIzaSyDUe0bor3TNIL0Nd4bXvwbAMjzNXn06CD0';
+
+  String? _token;
+  String? _email;
+  String? _userId;
+  DateTime? _expiryDate;
+
+  bool get isAuth {
+    final isValid = _expiryDate!.isAfter(DateTime.now());
+    return _token != null && isValid;
+  }
+
+  String? get token {
+    return isAuth ? _token : null;
+  }
+ 
+  String? get email {
+    return isAuth ? _email : null;
+  }
+ 
+  String? get userId {
+    return isAuth ? _userId : null;
+  }
 
   Future<void> _authenticate(String email, String password, String urlSegment) async {
     final url = urlSegment = '$_url$urlSegment$_keyUrlSegment';
@@ -24,6 +46,16 @@ class Auth with ChangeNotifier {
     final body = jsonDecode(response.body);
     if (body['error'] != null) {
       throw AuthException(body['error']['message']);
+    } else {
+      _token = body['idToken'];
+      _email = body['email'];
+      _userId = body['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(body['expiresIn']),
+        ),
+      );
+      notifyListeners();
     }
   }
 
